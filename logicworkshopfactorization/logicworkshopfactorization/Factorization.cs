a97455214010211085d5c54219f751f851b5aa7a
Tu Nombre <tu-email@ejemplo.com>
2026-04-25 15:50:49 -0500

    Factorization funcionando correctamente

---
+++ logicworkshopfactorization/logicworkshopfactorization/Factorization.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace LogicWorkshop;
+
+public class Factorization
+{
+    private int _number;
+
+    public Factorization(int number)
+    {
+        _number = number;
+    }
+
+    public string GetResult()
+    {
+        int n = _number;
+        int divisor = 2;
+        StringBuilder result = new StringBuilder();
+        result.Append($"{_number} ");
+
+        bool first = true;
+
+        while (n > 1)
+        {
+            if (n % divisor == 0)
+            {
+                if (!first)
+                {
+                    result.Append(" x ");
+                }
+                result.Append(divisor);
+                n /= divisor;
+                first = false;
+            }
+            else
+            {
+                divisor++;
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ logicworkshopfactorization/logicworkshopfactorization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LogicWorkshop;
+
+try
+{
+    Console.Write("Ingrese el número a descomponer: ");
+    int number = int.Parse(Console.ReadLine()!);
+
+    var factorization = new Factorization(number);
+    Console.WriteLine(factorization.GetResult());
+
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
